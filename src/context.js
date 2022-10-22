d6d2fe364f8a16720078e951d2f67164e80f4a70
kingbookk <kingbooktashkent@gmail.com>
2022-10-22 16:58:01 +0500

    new

--- src/context.js
+++ src/context.js
@@ -5,6 +5,29 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [coctails, setCoctails] = useState([]);
+
+  const fetchDrinnk = useCallback(async () => {
+    setLoading(true)
+    try {
+      const res = await fetch(`${url}${searchTerm}`);
+      const data = await res.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCoctails = drinks.map((item) => {
+          const {idDrink, }
+        })
+      }
+
+    } catch (error) {
+      
+    }
+  })
+
+
+
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
 // make sure use
